ce39fa1dceac89e1a36b5773c2a94fe605e9304d
Caslyn <caslyn.cole@gmail.com>
2017-02-15 10:31:07 -0800

    omit set-cookie on responses

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		expires_in 1.minutes, public: true
-		cookies.delete @_request
+		request.session_options[:skip] = true
 		render :index
 	end
 
